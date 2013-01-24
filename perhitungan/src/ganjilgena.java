434b75b1bbdc18737486bbc810ca0bcb5ba6a954
Fajar Putra <fajarnbozz@gmail.com>
2013-01-24 17:41:11 +0700

    jsp

---
+++ perhitungan/src/ganjilgena.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Pamungkas
+ */
+public class ganjilgena {
+    
+    public double bilangan;
+    public string komen;
+    
+    public void cekBilangan () {
+        if (bilangan %2== 0){
+            komen
+        }
+    }
+    
+}
